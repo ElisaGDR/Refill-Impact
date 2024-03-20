8963ad21be27c7d3e51dae2f241167476fd40531
Sahil Choujar <sahil.choujar@gmail.com>
2024-03-20 16:21:45 +0100

    now with router

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import router from '@/router';
+
+
+export default {
+  components: {
+   
+  },
+  data() {
+    return {
+     
+    };
+  },
+  methods: {
+  
+  },
+};
+</script>
+
+<style>
+/* Specific component styles */
+#app {
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+  font-size: 1.5em;
+}
+</style>
